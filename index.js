ab0e474d9bc1d1cc4a37e74badfbbc48ac918fde
Julian <105754345+jprune@users.noreply.github.com>
2022-07-14 01:23:52 +0200

    finished exercise

--- index.js
+++ index.js
@@ -1,71 +1,18 @@
 const inputEl = document.getElementById("unit-input")
 const buttonEl = document.getElementById("convert-btn")
-const resultContainerEl = document.getElementById("result-container")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
+
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
 
 buttonEl.addEventListener("click", function() {
-    //take input value and calculate in three variables
+
     const inputValue = inputEl.value
-    const length = renderLength(inputValue)
-    const volume = renderVolume(inputValue)
-    const mass = renderMass(inputValue)
 
-    resultContainerEl.innerHTML = length + volume + mass
+    lengthEl.textContent = `${inputValue} meter = ${(inputValue * meterToFeet).toFixed(3)} feet | ${inputValue} feet = ${(inputValue / meterToFeet).toFixed(3)} meter`
+    volumeEl.textContent = `${inputValue} liters = ${(inputValue * literToGallon).toFixed(3)} gallons | ${inputValue} gallons = ${(inputValue / literToGallon).toFixed(3)} liters`
+    massEl.textContent = `${inputValue} kilos = ${(inputValue * kiloToPound).toFixed(3)} pounds | ${inputValue} pounds = ${(inputValue / kiloToPound).toFixed(3)} kilos`
 })
-
-const renderLength = num => {
-    const feet = calcFeetFromMeter(num);
-    const meter = calcMeterFromFeet(num);
-    return `
-        <div class="result-container-single">
-            <h2 class="result-h2">Length (Meter/Feet)</h2>
-            <p class="result-p">${num} meters = ${feet} feet | ${num} feet = ${meter} meters</p>
-        </div>
-    `
-}
-const renderVolume = num => {
-    const gallons = calcGallonsFromLiter(num);
-    const liter = calcLitersFromGallons(num);
-    return `
-        <div class="result-container-single">
-            <h2 class="result-h2">Length (Gallons/Liter)</h2>
-            <p class="result-p">${num} liters = ${gallons} gallons | ${num} gallons = ${liter} liters</p>
-        </div>
-    `
-}
-const renderMass = num => {
-    const pounds = calcPoundsFromKilos(num);
-    const kilo = calcKilosFromPounds(num);
-    return `
-        <div class="result-container-single">
-            <h2 class="result-h2">Length (Gallons/Liter)</h2>
-            <p class="result-p">${num} kilos = ${pounds} pounds | ${pounds} pounds = ${kilo} kilos</p>
-        </div>
-    `
-}
-const calcFeetFromMeter = num => {
-    return Math.round((num * 3.821 + Number.EPSILON) * 1000) / 1000
-}
-const calcMeterFromFeet = num => {
-    return Math.round((num / 3.821 + Number.EPSILON) * 1000) / 1000
-}
-const calcGallonsFromLiter = num => {
-    return Math.round((num * 0.264 + Number.EPSILON) * 1000) / 1000
-}
-const calcLitersFromGallons = num => {
-    return Math.round((num / 0.264 + Number.EPSILON) * 1000) / 1000
-}
-const calcPoundsFromKilos = num => {
-    return Math.round((num * 2.204 + Number.EPSILON) * 1000) / 1000
-}
-const calcKilosFromPounds = num => {
-    return Math.round((num / 2.204 + Number.EPSILON) * 1000) / 1000
-}
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
-/*
-*  
-*/
